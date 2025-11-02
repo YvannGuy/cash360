62022e2ed9ed7b34415afe6795915e115365b512
yvann <yvann.guyonnet@gmail.com>
2025-11-02 21:34:43 +0100

    fix: suppression doublons fermetures JSX dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1001,9 +1001,6 @@ export default function DashboardPage() {
                 </div>
               )}
             </div>
-          )}
-            </div>
-          )}
         </div>
       </div>
 
